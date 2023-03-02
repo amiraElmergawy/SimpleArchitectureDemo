da8fbc8afe33bf7890577981a5396cd200dfcaf5
amiraElmergawy <amiraelmergawy@gmail.com>
2023-03-02 06:17:13 +0200

    Create UserDaoImpl.java

---
+++ src/main/java/com/amira/archeticture/demo/persistence/daos/impl/UserDaoImpl.java
@@ -0,0 +1,13 @@
+package com.amira.archeticture.demo.persistence.daos.impl;
+
+import com.amira.archeticture.demo.persistence.daos.UserDao;
+
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public boolean createUser() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    }
+
+}
