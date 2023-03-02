c429b1f0ec2e4e24484b1701aad4c74865e90df4
amiraElmergawy <amiraelmergawy@gmail.com>
2023-03-02 06:10:23 +0200

    Create OrderImpl.java

---
+++ src/main/java/com/amira/archeticture/demo/business/services/impl/OrderImpl.java
@@ -0,0 +1,14 @@
+package com.amira.archeticture.demo.business.services.impl;
+
+import com.amira.archeticture.demo.business.dtos.OrderDto;
+import com.amira.archeticture.demo.business.services.OrderService;
+
+public class OrderImpl implements OrderService{
+
+    @Override
+    public boolean addOrder(OrderDto order) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addOrder'");
+    }
+
+}
