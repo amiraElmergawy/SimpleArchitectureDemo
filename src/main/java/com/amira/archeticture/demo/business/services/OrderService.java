0c3ca6de4c6787a69e7b6cafe0e3aa756ff5dce3
amiraElmergawy <amiraelmergawy@gmail.com>
2023-03-02 06:10:35 +0200

    Create OrderService.java

---
+++ src/main/java/com/amira/archeticture/demo/business/services/OrderService.java
@@ -0,0 +1,7 @@
+package com.amira.archeticture.demo.business.services;
+
+import com.amira.archeticture.demo.business.dtos.OrderDto;
+
+public interface OrderService {
+    public boolean addOrder(OrderDto order);
+}
